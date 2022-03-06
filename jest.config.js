adbfe60009b156748df4b523a9ef83fc18652b77
Carol Ferreira <developer.anacarolina@gmail.com>
2022-03-06 20:44:44 -0300

    feat: config jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  testIgnorePatterns: ["/node_modules/", "/.next/"],
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
   setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
   },
   testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(scss|css|sass)$": "identity-obj-proxy",
+  },
 };
